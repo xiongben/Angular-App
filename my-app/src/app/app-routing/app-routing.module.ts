3d9fe0d8b2c188d29dfad66cf691b3c88d120a5a
xiongben <xiongben1993@gmail.com>
2019-10-16 18:29:50 +0800

    add

---
+++ my-app/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingComponent } from './app-routing.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+
+@NgModule({
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
